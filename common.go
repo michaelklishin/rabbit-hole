f3a26ac9d95e4d60ddbb9d0c03a89f5d1b538140
Michael Klishin <mklishin@gopivotal.com>
2013-11-05 00:35:41 +0400

    Docs

--- common.go
+++ common.go
@@ -1,13 +1,18 @@
 package rabbithole
 
+// Extra arguments as a map (on queues, bindings, etc)
 type Properties map[string]interface{}
 
+// Port used by RabbitMQ or clients
 type Port int
 
+// Rate of change of a numerical value
 type RateDetails struct {
 	Rate float32 `json:"rate"`
 }
 
+// RabbitMQ context (Erlang app) running on
+// a node
 type BrokerContext struct {
 	Node        string `json:"node"`
 	Description string `json:"description"`
@@ -16,6 +21,7 @@ type BrokerContext struct {
 	Ignore      bool   `json:"ignore_in_use"`
 }
 
+// Basic published messages statistics
 type MessageStats struct {
 	Publish        int         `json:"publish"`
 	PublishDetails RateDetails `json:"publish_details"`
