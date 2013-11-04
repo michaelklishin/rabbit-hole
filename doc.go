47973819355f75599a1535c66fc775a67c4fce45
Michael Klishin <mklishin@gopivotal.com>
2013-11-04 23:54:22 +0400

    Docs

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Rabbit Hole is a Go client for the RabbitMQ HTTP API.
+
+All HTTP API operations are accessible via `rabbithole.Client`, which
+should be instantiated with `rabbithole.NewClient`.
+*/ 
+package rabbithole
