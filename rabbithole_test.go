0d0f2ede92e32f45e2225177c4e61af7b15d1ba5
Michael Klishin <mklishin@gopivotal.com>
2013-11-05 23:32:21 +0400

    Support GET /api/nodes/{name}
    Fixes #52.

--- rabbithole_test.go
+++ rabbithole_test.go
@@ -56,7 +56,6 @@ var _ = Describe("Rabbithole", func() {
 					nil)
 				ch.Publish("", q.Name, false, false, amqp.Publishing{Body: []byte("")})
 			}
-			ch.Close()
 
 			res, err := rmqc.Overview()
 			Ω(err).Should(BeNil())
@@ -66,6 +65,8 @@ var _ = Describe("Rabbithole", func() {
 
 			fanoutExchange := ExchangeType{Name: "fanout", Description: "AMQP fanout exchange, as per the AMQP specification", Enabled: true}
 			Ω(res.ExchangeTypes).Should(ContainElement(fanoutExchange))
+
+			ch.Close()
 		})
 	})
 
@@ -105,6 +106,32 @@ var _ = Describe("Rabbithole", func() {
 		It("returns decoded response", func() {
 			xs, err := rmqc.ListNodes()
 			res := xs[0]
+
+			Ω(err).Should(BeNil())
+
+			Ω(res.Name).ShouldNot(BeNil())
+			Ω(res.NodeType).Should(Equal("disc"))
+
+			Ω(res.FdUsed).Should(BeNumerically(">=", 0))
+			Ω(res.FdTotal).Should(BeNumerically(">", 64))
+
+			Ω(res.MemUsed).Should(BeNumerically(">", 10*1024*1024))
+			Ω(res.MemLimit).Should(BeNumerically(">", 64*1024*1024))
+			Ω(res.MemAlarm).Should(Equal(false))
+
+			Ω(res.IsRunning).Should(Equal(true))
+
+			Ω(res.SocketsUsed).Should(BeNumerically(">=", 0))
+			Ω(res.SocketsTotal).Should(BeNumerically(">=", 1))
+
+		})
+	})
+
+	Context("GET /nodes/{name}", func() {
+		It("returns decoded response", func() {
+			xs, err := rmqc.ListNodes()
+			n := xs[0]
+			res, err := rmqc.GetNode(n.Name)
 
 			Ω(err).Should(BeNil())
 
