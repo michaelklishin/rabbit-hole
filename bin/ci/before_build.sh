8fabf70d52b689f7d7b09eaca4100ba88545072f
Michael Klishin <michael@clojurewerkz.org>
2016-12-01 03:30:02 +0300

    Experimenting with retention policy preconfiguration
    To make things pass against rabbitmq-management-236.

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -9,7 +9,8 @@ $RABBITHOLE_RABBITMQCTL add_user guest guest
 $RABBITHOLE_RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
 
 # Reduce retention policy for faster publishing of stats
-$RABBITHOLE_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+$RABBITHOLE_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+$RABBITHOLE_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
 
 $RABBITHOLE_RABBITMQCTL add_vhost "rabbit/hole"
 $RABBITHOLE_RABBITMQCTL set_permissions -p "rabbit/hole" guest ".*" ".*" ".*"
