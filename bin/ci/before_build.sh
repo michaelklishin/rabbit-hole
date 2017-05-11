92bf0f59bd0491e06fab47c6bddbf4283b3e9e69
Michael Klishin <michael@clojurewerkz.org>
2017-05-11 22:25:26 +0300

    Make this a bit less confusing

--- bin/ci/before_build.sh
+++ bin/ci/before_build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-RABBITHOLE_RABBITMQCTL=${RABBITHOLE_RABBITMQCTL:="sudo rabbitmqctl"}
-RABBITHOLE_RABBITMQ_PLUGINS=${RABBITHOLE_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+CTL=${RABBITHOLE_RABBITMQCTL:="sudo rabbitmqctl"}
+PLUGINS=${RABBITHOLE_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
 
 # guest:guest has full access to /
 
-$RABBITHOLE_RABBITMQCTL add_vhost /
-$RABBITHOLE_RABBITMQCTL add_user guest guest
-$RABBITHOLE_RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
+$CTL add_vhost /
+$CTL add_user guest guest
+$CTL set_permissions -p / guest ".*" ".*" ".*"
 
 # Reduce retention policy for faster publishing of stats
-$RABBITHOLE_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
-$RABBITHOLE_RABBITMQCTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
 
-$RABBITHOLE_RABBITMQCTL add_vhost "rabbit/hole"
-$RABBITHOLE_RABBITMQCTL set_permissions -p "rabbit/hole" guest ".*" ".*" ".*"
+$CTL add_vhost "rabbit/hole"
+$CTL set_permissions -p "rabbit/hole" guest ".*" ".*" ".*"
 
 # Enable shovel plugin
-$RABBITHOLE_RABBITMQ_PLUGINS enable rabbitmq_shovel
-$RABBITHOLE_RABBITMQ_PLUGINS enable rabbitmq_shovel_management
+$PLUGINS enable rabbitmq_shovel
+$PLUGINS enable rabbitmq_shovel_management
