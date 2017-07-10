e4c5f4181dc133273885904165a0092667f3e926
Michael Klishin <michael@clojurewerkz.org>
2017-07-10 12:56:15 +0300

    Pass ShellCheck

--- bin/ci/install_on_debian.sh
+++ bin/ci/install_on_debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /tmp/
+cd /tmp/ || exit 1
 wget https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_6_9/rabbitmq-server_3.6.9-1_all.deb
 sudo dpkg --install rabbitmq-server_3.6.9-1_all.deb
 sudo rm rabbitmq-server_3.6.9-1_all.deb
