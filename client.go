1e8fa75c235130745e7881c6afe294e80411afa9
Raymond Fallon <git@niclic.com>
2020-05-11 17:01:11 -0400

    Named result should be the value returned.

--- client.go
+++ client.go
@@ -102,14 +102,14 @@ func newRequestWithBody(client *Client, method string, path string, body []byte)
 	return req, err
 }
 
-func executeRequest(client *Client, req *http.Request) (res *http.Response, err error) {
+func executeRequest(client *Client, req *http.Request) (resp *http.Response, err error) {
 	httpc := &http.Client{
 		Timeout: client.timeout,
 	}
 	if client.transport != nil {
 		httpc.Transport = client.transport
 	}
-	resp, err := httpc.Do(req)
+	resp, err = httpc.Do(req)
 	if err != nil {
 		return nil, err
 	}
