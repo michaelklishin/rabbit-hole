88899b9032bff2bc1563547493efd1369acf3558
Michael Klishin <michael@clojurewerkz.org>
2020-12-09 14:57:44 +0300

    Move /api/whoami endpoint func to users.go

--- users.go
+++ users.go
@@ -240,3 +240,29 @@ func Base64EncodedSaltedPasswordHashSHA512(password string) string {
 	salt, saltedHash := SaltedPasswordHashSHA512(password)
 	return base64.StdEncoding.EncodeToString([]byte(salt + saltedHash))
 }
+
+//
+// GET /api/whoami
+//
+
+// WhoamiInfo represents a user whose request was successfully authenticated
+// by the "whoami" API endpoint.
+type WhoamiInfo struct {
+	Name        string   `json:"name"`
+	Tags        UserTags `json:"tags"`
+	AuthBackend string   `json:"auth_backend"`
+}
+
+// Whoami echoes requesting user's name back.
+func (c *Client) Whoami() (rec *WhoamiInfo, err error) {
+	req, err := newGETRequest(c, "whoami")
+	if err != nil {
+		return nil, err
+	}
+
+	if err = executeAndParseRequest(c, req, &rec); err != nil {
+		return nil, err
+	}
+
+	return rec, nil
+}
