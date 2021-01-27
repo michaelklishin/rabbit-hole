a20ad1eeb90b5015a79391638551cd9700c0b06f
Martin Krueger <martin.krueger@sabio.de>
2021-01-27 14:07:49 +0100

    update comments

--- health_checks.go
+++ health_checks.go
@@ -34,7 +34,7 @@ type Check interface {
 	Failed() bool
 }
 
-// Health represents response from healthchecks endpoint
+// Represents general response from health check endpoints if no dedicated representation is defined
 type Health struct {
 	Check
 	Status string `json:"status"`
@@ -49,20 +49,19 @@ func (h *Health) Failed() bool {
 	return !h.Ok()
 }
 
-// Responds a 200 OK if there are no alarms in effect in the cluster, otherwise responds with a 503 Service Unavailable.
+// Checks if there are alarms in effect in the cluster
 func (c *Client) HealthCheckAlarms() (rec Health, err error) {
 	err = executeCheck(c, "health/checks/alarms", &rec)
 	return rec, err
 }
 
-// Responds a 200 OK if there are no local alarms in effect on the target node, otherwise responds with a 503 Service Unavailable.
+// Checks if there are local alarms in effect on the target node
 func (c *Client) HealthCheckLocalAlarms() (rec Health, err error) {
 	err = executeCheck(c, "health/checks/local-alarms", &rec)
 	return rec, err
 }
 
 // Checks the expiration date on the certificates for every listener configured to use TLS.
-// Responds a 200 OK if all certificates are valid (have not expired), otherwise responds with a 503 Service Unavailable.
 // Valid units: days, weeks, months, years. The value of the within argument is the number of units.
 // So, when within is 2 and unit is "months", the expiration period used by the check will be the next two months.
 func (c *Client) HealthCheckCertificateExpiration(within uint, unit TimeUnit) (rec Health, err error) {
@@ -70,21 +69,23 @@ func (c *Client) HealthCheckCertificateExpiration(within uint, unit TimeUnit) (r
 	return rec, err
 }
 
+// Represents the response from HealthCheckPortListener
 type PortListenerHealth struct {
 	Check
 	Status  string `json:"status"`
 	Reason  string `json:"reason"`
-	Missing string `json:"missing"'`
+	Missing string `json:"missing"`
 	Port    uint   `json:"port"`
 	Ports   []uint `json:"ports"`
 }
 
-// Responds a 200 OK if there is an active listener on the give port, otherwise responds with a 503 Service Unavailable.
+// Checks if there is an active listener on the give port
 func (c *Client) HealthCheckPortListener(port uint) (rec PortListenerHealth, err error) {
 	err = executeCheck(c, "health/checks/port-listener/"+strconv.Itoa(int(port)), &rec)
 	return rec, err
 }
 
+// Represents the response from HealthCheckProtocolListener
 type ProtocolListenerHealth struct {
 	Check
 	Status    string   `json:"status"`
@@ -93,28 +94,26 @@ type ProtocolListenerHealth struct {
 	Protocols []string `json:"protocols"`
 }
 
-// Responds a 200 OK if there is an active listener for the given protocol, otherwise responds with a 503 Service Unavailable.
+// Checks if there is an active listener for the given protocol
 // Valid protocol names are: amqp091, amqp10, mqtt, stomp, web-mqtt, web-stomp.
 func (c *Client) HealthCheckProtocolListener(protocol Protocol) (rec ProtocolListenerHealth, err error) {
 	err = executeCheck(c, "health/checks/protocol-listener/"+string(protocol), &rec)
 	return rec, err
 }
 
-// Responds a 200 OK if all virtual hosts and running on the target node, otherwise responds with a 503 Service Unavailable.
+// Checks if all virtual hosts and running on the target node
 func (c *Client) HealthCheckVirtualHosts() (rec Health, err error) {
 	err = executeCheck(c, "health/checks/virtual-hosts", &rec)
 	return rec, err
 }
 
 // Checks if there are classic mirrored queues without synchronised mirrors online (queues that would potentially lose data if the target node is shut down).
-// Responds a 200 OK if there are no such classic mirrored queues, otherwise responds with a 503 Service Unavailable.
 func (c *Client) HealthCheckNodeIsMirrorSyncCritical() (rec Health, err error) {
 	err = executeCheck(c, "health/checks/node-is-mirror-sync-critical", &rec)
 	return rec, err
 }
 
 // Checks if there are quorum queues with minimum online quorum (queues that would lose their quorum and availability if the target node is shut down).
-// Responds a 200 OK if there are no such quorum queues, otherwise responds with a 503 Service Unavailable.
 func (c *Client) HealthCheckNodeIsQuorumCritical() (rec Health, err error) {
 	err = executeCheck(c, "health/checks/node-is-quorum-critical", &rec)
 	return rec, err
