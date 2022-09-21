e8118c853f3adecf72a6b535ec26bde7c7879b32
Mirah Gary <mgary@vmware.com>
2022-09-21 15:48:48 +0200

    Set default async test timeout to 5s.

--- rabbithole_suite_test.go
+++ rabbithole_suite_test.go
@@ -2,6 +2,7 @@ package rabbithole
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,5 +10,6 @@ import (
 
 func TestRabbitHole(t *testing.T) {
 	RegisterFailHandler(Fail)
+	SetDefaultEventuallyTimeout(5*time.Second)
 	RunSpecs(t, "Rabbithole Suite")
 }
